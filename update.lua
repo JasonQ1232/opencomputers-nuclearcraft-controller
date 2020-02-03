eaaf4dbbeb193d4ba690cac706f8688d49bc4bae
JasonQ1232 <Jason@jason1232.uk>
2020-02-03 03:42:45 +0000

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,4 +1,4 @@
 os = require("os")
 
-os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/rc_installer.lua /home/rc_installer.lua")
-os.execute("/home/rc_installer.lua")
+os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/rc_installer.lua /home/reactor_control/rc_installer.lua")
+os.execute("/home/reactor_control/rc_installer.lua")
