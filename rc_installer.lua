bf68f09e9254eb8bbd7c283de36a0062d8fe1c79
JasonQ1232 <Jason@jason1232.uk>
2020-02-03 03:27:48 +0000

    Update rc_installer.lua

--- rc_installer.lua
+++ rc_installer.lua
@@ -3,7 +3,7 @@ os = require("os")
 os.execute("mkdir /etc/rc.d/reactor_control/")
 os.execute("mkdir /home/reactor_control")
 
-os.execute("mv rc_installer.lua /home/reactor_control/rc_installer.lua"
+os.execute("mv rc_installer.lua /home/reactor_control/rc_installer.lua")
 
 os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/lib/reactor.lua /usr/lib/reactor_control/reactor.lua")
 os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/reactor_controller.lua /etc/rc.d/reactor_control/controller.lua")
