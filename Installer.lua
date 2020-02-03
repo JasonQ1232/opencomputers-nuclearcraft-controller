cd19d241044f3b0ae85c8ccad2e15a006abbe1e9
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2020-02-03 03:01:13 +0000

    Delete Installer.lua

--- Installer.lua
+++
@@ -1,8 +0,0 @@
-os = require("os")
-
-os.execute("mkdir /usr/lib/reactor_control")
-os.execute("mkdir /ect/rc.d/reactor_control")
-os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/lib/reactor.lua /usr/reactor_control/lib")
-os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/ReactorControl.lua /etc/rc.d/reactor_control")
-os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/ReactorRelay.lua /etc/rc.d/reactor_control")
-os.execute("wget -f https://github.com/JasonQ1232/opencomputers-nuclearcraft-controller/raw/master/ReactorListener.lua /etc/rc.d/reactor_control")
